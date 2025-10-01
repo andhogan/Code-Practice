40187c213062899aae1c7462f1fe97d6064348a2
ahogan2 <ahogan2@blueorigin.com>
2025-10-01 13:41:46 -0600

    1.7: unicafe step 2
    Signed-off-by: ahogan2 <ahogan2@blueorigin.com>

--- Coursework/fullstackopen/part1/unicafe/src/App.jsx
+++ Coursework/fullstackopen/part1/unicafe/src/App.jsx
@@ -11,10 +11,18 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let all = good+neutral+bad
+  let average = (good-bad)/(all)
+  let positive = ((good/all) * 100).toString() + ' %'
 
   const increaseGoodByOne = () => setGood(good + 1)
   const increaseNeutralByOne = () => setNeutral(neutral + 1)
   const increaseBadByOne = () => setBad(bad + 1)
+  const resetToZero = () => {
+    setGood(0)
+    setNeutral(0)
+    setBad(0)
+  }
 
   return (
     <div>
@@ -22,10 +30,14 @@ const App = () => {
       <Button onClick={increaseGoodByOne} text='good'/>
       <Button onClick={increaseNeutralByOne} text='neutral'/>
       <Button onClick={increaseBadByOne} text='bad'/>
+      <Button onClick={resetToZero} text='reset'/>
       <Header text='statistics'/>
       <Display text='good' counter={good} />
       <Display text='neutral' counter={neutral} />
       <Display text='bad' counter={bad} />
+      <Display text='all' counter={good+neutral+bad} />
+      <Display text='average' counter={average} />
+      <Display text='positive' counter={positive} />
     </div>
   )
 }
